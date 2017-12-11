c731b248b8677c5a867bcb9e51d503e960de28c3
Marcis <marcis.zoldners@gmail.com>
2017-12-11 21:25:51 +0000

    Creating topics

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Http\Request;
 
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::post('/register', 'RegisterController@register');
+
+Route::group(['prefix' => 'topics'], function () {
+    Route::post('/', 'TopicController@store')->middleware('auth:api');
+});
