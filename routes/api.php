768b281a4e59479a94ee192c709babacaeb660d6
Marcis Zoldners <marcis.zoldners@gmail.com>
2017-12-17 23:06:41 +0200

    Showing a topic

--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,6 @@ Route::post('/register', 'RegisterController@register');
 
 Route::group(['prefix' => 'topics'], function () {
     Route::get('/', 'TopicController@index');
+    Route::get('/{topic}', 'TopicController@show');
     Route::post('/', 'TopicController@store')->middleware('auth:api');
 });
